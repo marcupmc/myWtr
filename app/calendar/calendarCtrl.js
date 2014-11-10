11b1de25760d50430847d41c8a516d5c59a1c1c8
marcgregoire <marc.gregoire1989@gmail.com>
2014-11-10 12:14:19 +0100

    Add DateTime picker
    Time picker + Design

--- app/calendar/calendarCtrl.js
+++ app/calendar/calendarCtrl.js
@@ -1,9 +1,9 @@
 angular.module('myWtrApp')
   .controller('calendarCtrl',['$scope', function($scope) {
 
-    $scope.eventSources =
-
+    $scope.eventSources = [];
     $scope.afficherFormulaireActivite=false;
+
 
     $scope.ajouterActivite = function(){
       $scope.afficherFormulaireActivite=true;
@@ -14,4 +14,43 @@ angular.module('myWtrApp')
 
       //Utiliser un service d'ajout au calendrier dans service.js
     }
+
+
+    //Options pour le calendrier => a mettre dans un service?
+    $scope.today = function() {
+        $scope.dt = new Date();
+    };
+    $scope.today();
+
+    $scope.clear = function () {
+      $scope.dt = null;
+    };
+
+    // Disable weekend selection
+    $scope.disabled = function(date, mode) {
+      return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+    };
+
+    $scope.toggleMin = function() {
+      $scope.minDate = $scope.minDate ? null : new Date();
+    };
+    $scope.toggleMin();
+
+    $scope.openStart = function($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+      $scope.openedStart = true;
+    };
+
+    $scope.openEnd = function($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+      $scope.openedEnd = true;
+    };
+
+    $scope.dateOptions = {
+      formatYear: 'yy',
+      startingDay: 1
+    };
+    $scope.format = 'dd/MM/yyyy';
 }]);
